d51488c7726c423c798999f86bb420bb6a971023
VlexandreMG <dodolashmou@gmail.com>
2026-06-25 15:03:43 +0300

    TestController.java doonneeee

--- demo/src/main/java/entrepot/demo/controller/TestController.java
+++ demo/src/main/java/entrepot/demo/controller/TestController.java
@@ -1,5 +1,15 @@
 package entrepot.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TestController {
-    
+
+    @GetMapping("/test")
+    public String tester(Model model) {
+        model.addAttribute("monMessage", "Si tu vois ce message, ton architecture est parfaite !");
+        return "test";
+    }
 }
