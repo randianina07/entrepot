1e69542b88c702695e8e949c1f37e0aa5fd6640e
VlexandreMG <dodolashmou@gmail.com>
2026-07-02 09:58:42 +0300

    Ajout dans la classe

--- demo/src/main/java/entrepot/demo/service/EmplacementService.java
+++ demo/src/main/java/entrepot/demo/service/EmplacementService.java
@@ -1,44 +1,23 @@
 package entrepot.demo.service;
 
 import entrepot.demo.model.Emplacement;
+import entrepot.demo.repositories.EmplacementRepository;
+
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
+import entrepot.demo.repositories.EmplacementRepository;
 
 @Service
 public class EmplacementService {
 
-    // Cette méthode simule ta base de données pour le MVP
-    private List<Emplacement> genererFauxEmplacements() {
-        List<Emplacement> liste = new ArrayList<>();
-
-        // On crée 3 faux emplacements pour tester les cas de figure
-        Emplacement emp1 = new Emplacement();
-        emp1.setCode("ETA-A1-ET1-E1");
-        emp1.setCapacite_volume_m3(10.0); // Trop petit pour ton produit de 20
-        emp1.setActif(true);
-
-        Emplacement emp2 = new Emplacement();
-        emp2.setCode("ETA-A1-ET1-E2");
-        emp2.setCapacite_volume_m3(30.0);
-        emp2.setActif(true); // Assez grand, mais déjà occupé/inactif !
-
-        Emplacement emp3 = new Emplacement();
-        emp3.setCode("ETA-A1-ET1-E3");
-        emp3.setCapacite_volume_m3(35.0);
-        emp3.setActif(true); // Parfait ! Libre et assez grand.
-
-        liste.add(emp1);
-        liste.add(emp2);
-        liste.add(emp3);
-        return liste;
-    }
+    private EmplacementRepository emplacementRepository;
 
     // Le cœur de ton MVP : l'algorithme de recherche rapide
     public List<Emplacement> trouverPlaceRapide(double tailleProduit, int quantite) {
         int count = 0;
         List<Emplacement> listeEmplacementsTrouves = new ArrayList<>();
-        List<Emplacement> tousLesEmplacements = genererFauxEmplacements();
+        List<Emplacement> tousLesEmplacements = emplacementRepository.findAll();
 
         // On parcourt la liste (Vô mitady libre ray)
         while (count != quantite) {
