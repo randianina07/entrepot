6f6609cce5149ece2f484d44b0bf3e04fe5cd067
VlexandreMG <dodolashmou@gmail.com>
2026-07-02 14:42:21 +0300

    Tsy  mety le verificateur

--- demo/src/main/java/entrepot/demo/service/EmplacementService.java
+++ demo/src/main/java/entrepot/demo/service/EmplacementService.java
@@ -16,35 +16,20 @@ public class EmplacementService {
 
     // Le cœur de ton MVP : l'algorithme de recherche rapide
     public List<Emplacement> trouverPlaceRapide(double tailleProduit, int quantite) {
-        int count = 0;
         List<Emplacement> listeEmplacementsTrouves = new ArrayList<>();
         List<Emplacement> tousLesEmplacements = emplacementRepository.findAll();
 
-        // On parcourt la liste (Vô mitady libre ray)
-        while (count != quantite) {
-            boolean trouveDansCeTour = false;
+        // On parcourt la liste (Vô mitady libre ra
             for (Emplacement emp : tousLesEmplacements) {
-                // Règle métier : Doit être actif ET assez grand (taille < capacité)
-                if (emp.isActif() && emp.getCapacite_volume_m3() >= tailleProduit) {
-                    double verifVolum = tailleProduit - emp.getCapacite_volume_m3();
-                    if (verifVolum < 0) {
-                        continue;
-                    } else {
-                        listeEmplacementsTrouves.add(emp);
-                        trouveDansCeTour = true;
-                        count++;
-                    }
-
-                    if (count == quantite) {
-                        break;
-                    }
-                }
-
-                if (trouveDansCeTour) {
+                //Quand la liste est égale à la quantite demandé 
+                if (listeEmplacementsTrouves.size() == quantite) {
                     break;
                 }
+                // Règle métier : Doit être actif ET assez grand (taille < capacité)
+                if (emp.isActif() && emp.getCapacite_volume_m3() >= tailleProduit) {
+                        listeEmplacementsTrouves.add(emp);
+                }
             }
-        }
         return listeEmplacementsTrouves; // Renvoie null si aucune place ne correspond
     }
 }
