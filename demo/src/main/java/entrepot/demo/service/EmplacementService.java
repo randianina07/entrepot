69964e70089cf8a227c59a8839332dc99d7f3675
VlexandreMG <dodolashmou@gmail.com>
2026-07-03 00:32:52 +0300

    Algo avec l'allee

--- demo/src/main/java/entrepot/demo/service/EmplacementService.java
+++ demo/src/main/java/entrepot/demo/service/EmplacementService.java
@@ -1,9 +1,11 @@
 package entrepot.demo.service;
 
+import entrepot.demo.model.Allees;
 import entrepot.demo.model.Emplacement;
 import entrepot.demo.model.Etage;
 import entrepot.demo.repositories.EmplacementRepository;
 import entrepot.demo.repositories.EtageRepository;
+import entrepot.demo.repositories.AlleeRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,9 @@ import java.util.List;
 
 @Service
 public class EmplacementService {
+
+    @Autowired
+    private AlleeRepository alleeRepository;
 
     @Autowired
     private EtageRepository etageRepository;
@@ -23,27 +28,30 @@ public class EmplacementService {
     public List<Emplacement> trouverPlaceRapide(double tailleProduit, int quantite) {
         // Liste d'emplacements
         List<Emplacement> tousLesEmplacements = emplacementRepository.findAll();
-        List<Emplacement> listeEmplacementsTrouves = new ArrayList<>();
-
         // Liste d'étages
         List<Etage> tousLesEtages = etageRepository.findAll();
-        for (Etage etage : tousLesEtages) {
-            // On parcourt la liste (Vô mitady libre ra
-            for (Emplacement emp : tousLesEmplacements) {
+        // Liste d'alleees
+        List<Allees> tousLesAllees = alleeRepository.findAll();
+
+        List<Emplacement> listeEmplacementsTrouves = new ArrayList<>();
+        for (Allees allee : tousLesAllees) {
+            for (Etage etage : tousLesEtages) {
+                for (Emplacement emp : tousLesEmplacements) {
+                    if (listeEmplacementsTrouves.size() == quantite) {
+                        break;
+                    }
+                    if (allee.getId().equals(emp.getAllee().getId()) && emp.getEtage() != null && emp.getEtage().getId().equals(etage.getId())) {
+                        // Règle métier : Doit être actif ET assez grand (taille < capacité)
+                        if (emp.isActif() && emp.getCapacite_volume_m3() >= tailleProduit) {
+                            listeEmplacementsTrouves.add(emp);
+                        }
+                    }
+
+                }
+
                 if (listeEmplacementsTrouves.size() == quantite) {
                     break;
                 }
-                if (emp.getEtage() != null && emp.getEtage().getId().equals(etage.getId())) {
-                    // Règle métier : Doit être actif ET assez grand (taille < capacité)
-                    if (emp.isActif() && emp.getCapacite_volume_m3() >= tailleProduit) {
-                        listeEmplacementsTrouves.add(emp);
-                    }
-                }
-
-            }
-
-            if (listeEmplacementsTrouves.size() == quantite) {
-                break;
             }
         }
 
