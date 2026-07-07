3b8513b60ff8da0d5c30a40646d49a361594c3d0
RAOYo <yoannrah12@outlook.com>
2026-07-07 23:29:31 +0300

    refactor code

--- demo/src/main/java/entrepot/demo/model/TarifZone.java
+++ demo/src/main/java/entrepot/demo/model/TarifZone.java
@@ -13,12 +13,9 @@ public class TarifZone {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // NOTE : type_zone_id est NOT NULL dans la table, mais la table
-    // "types_zone" n'a pas ete fournie dans le schema que tu m'as donne
-    // (seule la contrainte FK vers types_zone(id) apparait). Je laisse donc
-    // l'id brut en attendant que tu me donnes le CREATE TABLE types_zone.
-    @Column(name = "type_zone_id", nullable = false)
-    private Long typeZoneId;
+    @ManyToOne
+    @JoinColumn(name = "type_zone_id", nullable = false)
+    private TypeZone typeZone;
 
     @ManyToOne
     @JoinColumn(name = "unite_duree_id", nullable = false)
@@ -44,12 +41,12 @@ public class TarifZone {
         this.id = id;
     }
 
-    public Long getTypeZoneId() {
-        return typeZoneId;
+    public TypeZone getTypeZone() {
+        return typeZone;
     }
 
-    public void setTypeZoneId(Long typeZoneId) {
-        this.typeZoneId = typeZoneId;
+    public void setTypeZone(TypeZone typeZone) {
+        this.typeZone = typeZone;
     }
 
     public UniteDuree getUniteDuree() {
