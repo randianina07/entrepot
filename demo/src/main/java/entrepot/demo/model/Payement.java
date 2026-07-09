9024961e35ebc2c624ba0dcc6b80115ae4c4c38b
hatif <fitahiana.andrianantenaina@gmail.com>
2026-07-09 13:20:18 +0300

    commit

--- demo/src/main/java/entrepot/demo/model/Payement.java
+++ demo/src/main/java/entrepot/demo/model/Payement.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -14,8 +16,14 @@ public class Payement {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     long id;
 
-    long client_id;
+    @ManyToOne
+    @JoinColumn(name = "client_id", referencedColumnName = "id")
+    Utilisateur client;
+
     double prix_payer;
-    int id_mode_de_payement;
+    
+    @ManyToOne
+    @JoinColumn(name = "id_mode_de_payement" , referencedColumnName = "id")
+    Mode_payement mode_de_payement;
 
 }
---
+++ demo/src/main/java/entrepot/demo/model/Role.java
@@ -0,0 +1,35 @@
+package entrepot.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "role")
+public class Role {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    String libelle;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+    
+}
