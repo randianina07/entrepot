d2d723a964e30c316e4eb2927e3d88b8ffede1ff
RAVELOMANANTSOA Ny Sanda Itokiana <itokianara3@gmail.com>
2026-07-09 13:50:10 +0300

    modif vitsy vitsy

--- src/main/java/com/entrepot/gestion/model/StatsClient.java
+++ src/main/java/com/entrepot/gestion/model/StatsClient.java
@@ -1,9 +1,18 @@
 package com.entrepot.gestion.model;
 
-import jakarta.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "stats_clients")
--- src/main/java/com/entrepot/gestion/model/StatutMouvement.java
+++ src/main/java/com/entrepot/gestion/model/StatutMouvement.java
@@ -1,7 +1,13 @@
 package com.entrepot.gestion.model;
 
-import jakarta.persistence.*;
 import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "statuts_mouvement")
