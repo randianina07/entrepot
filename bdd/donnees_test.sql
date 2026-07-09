8002b9865835201390e4b6390369105f5890f0b1
Hlee2705 <randrianasolonomenavalisoa@gmail.com>
2026-07-09 10:13:30 +0300

    tsy aiko

---
+++ bdd/donnees_test.sql
@@ -0,0 +1,29 @@
+-- Création de l'utilisateur administrateur
+INSERT INTO utilisateurs (
+    email,
+    mot_de_passe_hash,
+    role_id
+)
+VALUES (
+    'admin@entrepot.com',
+    '$2a$10$7EqJtq98hPqEX7fNZaFWoOHiKqG5q6QeF1J7P2QJm2d7l5z0QvM6C',
+    '1'
+);
+
+-- Informations de l'administrateur
+INSERT INTO utilisateurs_info (
+    utilisateur_id,
+    nom,
+    prenom,
+    numero,
+    adresse,
+    secteur
+)
+VALUES (
+    (SELECT id FROM utilisateurs WHERE email = 'admin@entrepot.com'),
+    'Admin',
+    'Super',
+    '0340000000',
+    'Antananarivo',
+    'Administration'
+);
